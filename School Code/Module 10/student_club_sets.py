0040fbb6981d51201d68b8db4cd834daedcb9585
Cay-tech7 <caydenpeastman@gmail.com>
2025-11-03 16:12:20 -0700

    edits

---
+++ School Code/Module 10/student_club_sets.py
@@ -0,0 +1,25 @@
+top_gun_club = {"Maverick","Goose","Iceman","Viper"}
+awaks_support_club = {"Viper","Hangman","Bob","DaggerII"}
+
+#finds the pritns a list of studetns who are in both clubs
+def both_clubs():
+    pass
+
+#finds and prints stuends who are in only one of the clubs
+def only_one_club():
+    pass
+
+#finds students who are only in the Top Gun club
+def only_top_gun_club():
+    pass
+
+#finds and prints all unique students across both clubs
+def all_unique_students():
+    pass
+
+if __name__ == "__main__":
+
+    #add new member "grace to the Top Gun club
+
+    #removes "bob" from the AWAKS Support club
+    pass
